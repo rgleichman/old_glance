2d77998b9c21d5d2cddb088c1d6be5f8d00dc424
Robbie Gleichman <rgleichman@gmail.com>
2015-03-14 23:02:00 -0700

    Added connection ports to apply triangle. Switched to monospace font.

--- diagrams/Apply.hs
+++ diagrams/Apply.hs
@@ -9,11 +9,10 @@ import qualified "SVGFonts" Graphics.SVGFonts as SVG
 -- If reverts are slow, then M-x customize-variable auto-revert-interval
 
 --TODO: figure out how to not have the lines and text overlap with round end-caps
--- TODO: Don't use angles (connectPerim') for triangle
 
 -- MAIN --
 main :: IO()
-main = mainWith example
+main = mainWith (example # frame 0.1)
 
 -- TOP LEVEL DIAGRAMS --
 example :: Diagram B R2
@@ -21,11 +20,12 @@ example = e1WithLines
           
 e1WithLines :: Diagram B R2
 e1WithLines = manualLayout #
-              connectNoHead red "f1" "t1" #
-              (lc black $ connectPerim' (with & arrowHead .~ noHead) "t1" "y1" (1/2 @@ turn) (0 @@ turn)) #
-              (lc blue $ connectPerim' (with & arrowHead .~ noHead) "x1" "t1" (1/4 @@ turn) (-91 @@ deg)) #lwL 0.1
+              connectNoHead red "f1" "t1in" #
+              (lc black $ connectOutside' (with & arrowHead .~ noHead) "t1out" "y1") #
+              (lc blue $ connectOutside' (with & arrowHead .~ noHead & tailGap .~ small) "x1" "t1l") #lwL 0.1
 
-manualLayout = position (zip (map p2 [(-0.5,0.1), (1,0), (2,-0.1), (1.2, -1)]) [y1, t1, f1, x1])
+--manualLayout = position (zip (map p2 [(-0.4,0.1), (1,0), (1.5,0.1), (1.1, -1)]) [y1, t1, f1, x1])
+manualLayout = position (zip (map p2 [(-0.4,0), (1,0), (1.5,0), (1, -1)]) [y1, t1, f1, x1])
 
 -- ICONS --
 t1 = applyTriangle blue "t1"
@@ -36,12 +36,15 @@ x1 = textInBox "x" "x1"
 -- SYMBOL FUNCTIONS --
 
 -- ?? Why does line cap style not work here for the vrule?
-applyTriangle color name = (vrule 1 # lwL 0.1 # lc color) <> triangle 1 # fc green # lw none # rotateBy (1/4) # alignR  # named name
+--applyTriangle :: (Monoid m, Semigroup m, TrailLike (QDiagram b R2 m), IsName n) => Colour Double -> n -> QDiagram b R2 m
+applyTriangle color name = (vrule 1 # lwL 0.1 # lc color) <> baseTri # fc lime # lw none # named name <> decoratePath baseTri (map (\x -> mempty # named (name ++ x)) ["r", "out", "l"]) <> mempty # named (name ++ "in")
+  where
+    baseTri = triangle 1 # rotateBy (1/4) # alignR
 
 textInBox ::  IsName n => String -> n -> Diagram B R2
 textInBox letters name = t # stroke # fc black # lw none <> (boundingRect t # lw none) # named name
   where
-    t = SVG.textSVG letters 1
+    t = SVG.textSVG' $ SVG.TextOpts letters SVG.bit SVG.INSIDE_H SVG.KERN False 1 1
 
 connectNoHead :: (IsName n1, IsName n2) => Colour Double -> n1 -> n2 -> Diagram B R2 -> Diagram B R2
-connectNoHead color = connectOutside' (with & arrowHead .~ noHead) # lc color
+connectNoHead color = connectOutside' (with & arrowHead .~ noHead & gaps .~ small) # lc color
